d904539d67a3e28da36c36a3c200c32496333e84
Adán SDPC <adan.sdpc@gmail.com>
2021-07-18 14:29:56 +0200

    deploy

---
+++ _nuxt/c26aa86.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{750:function(n,e,d){"use strict";d.r(e);var l=d(45),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1626611391/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626611391",layout:"default",error:a,serverRendered:b,routePath:c,config:{app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
